eba111815445b7389ea9878cd1a1f8363fd57d05
jasper1001 <jasper9330@gmail.com>
2016-08-05 14:23:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
+
+
+
 <link rel ="stylesheet" href="style.css"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<?php
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+     <?php
      echo "<table>";
      echo "<tr>";
      echo "<td class='tabledata'>Name</td>";
      echo "<td>Jasper</td>";
      echo "</tr>";
      echo "</table>";
-?>
+		?>
+    </p>
+  </body>
+</html>
